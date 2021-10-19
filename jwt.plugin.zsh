2318925d6fb58628444d3abb6e763e2791c86473
Alexandru Rosianu <me@aluxian.com>
2021-10-19 10:31:37 +0100

    add function

---
+++ jwt.plugin.zsh
@@ -0,0 +1,27 @@
+function jwt {
+    local usage="jwt header.json payload.json private.key"
+
+    if [[ $# < 2 || -n "${*[(r)--help]}" ]]; then
+        echo "$usage"
+        return 1
+    fi
+
+    if [[ $(jq -r '.alg' header.json) != "RS256" ]]; then
+        echo "only RS256 is supported at the moment"
+        return 1
+    fi
+
+    local headerJSON="$1"
+    local payloadJSON="$2"
+    local privateKEY="$3"
+    shift 3
+
+    local headerBase64url=$(cat "$headerJSON" | base64 | tr '+/' '-_' | tr -d '=')
+    local payloadBase64url=$(cat "$payloadJSON" | base64 | tr '+/' '-_' | tr -d '=')
+    
+    local claim="$headerBase64url.$payloadBase64url"
+
+    local sigBase64url=$(echo -n "$claim" | openssl dgst -sha256 -sign "$privateKEY" | openssl enc -base64 | tr -- '+/' '-_' | tr -d '\n=')
+
+    echo "$headerBase64url.$payloadBase64url.$sigBase64url"
+}
